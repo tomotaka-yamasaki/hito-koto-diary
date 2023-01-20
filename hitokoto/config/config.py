1fa68eb2f89b3478df92c9ded13fdfec2b288dfc
tomotaka-yamasaki <tomotaka.yamasaki@aktsk.jp>
2023-01-20 18:15:21 +0900

    feat: Linter, Formatterの導入

--- hitokoto/config/config.py
+++ hitokoto/config/config.py
@@ -1,9 +1,10 @@
 import os
-from os.path import join, dirname
+from os.path import dirname, join
+
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
-SA_KEY_PATH = os.environ.get('SA_KEY_PATH')
-SPREADSHEET_ID = os.environ.get('SPREADSHEET_ID')
+SA_KEY_PATH = os.environ.get("SA_KEY_PATH")
+SPREADSHEET_ID = os.environ.get("SPREADSHEET_ID")
--- hitokoto/google/spreadsheets.py
+++ hitokoto/google/spreadsheets.py
@@ -1,72 +1,80 @@
-import pickle
 import os.path
-from httplib2 import Http
+import pickle
+
 from googleapiclient.discovery import build
+from httplib2 import Http
 from oauth2client.service_account import ServiceAccountCredentials
 
-class GoogleSpreadSheet():
+
+class GoogleSpreadSheet:
     def __init__(self, spreadsheet_id, cred_dir):
         self.service = self.authorize_service(cred_dir)
         self.spreadsheet_id = spreadsheet_id
         self.spreadsheets = self.service.spreadsheets().get(spreadsheetId=self.spreadsheet_id).execute()
 
     def authorize_service(self, cred_dir):
-        SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+        scopes = ["https://www.googleapis.com/auth/spreadsheets"]
 
         creds = None
-        pickle_path = os.path.join(cred_dir, 'token.pickle')
+        pickle_path = os.path.join(cred_dir, "token.pickle")
         if os.path.exists(pickle_path):
-            with open(pickle_path, 'rb') as token:
+            with open(pickle_path, "rb") as token:
                 creds = pickle.load(token)
         if not creds:
             # credentials.json was created by GCP service account
-            cred_path = os.path.join(cred_dir, 'credentials.json')
-            creds = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scopes=SCOPES)
+            cred_path = os.path.join(cred_dir, "credentials.json")
+            creds = ServiceAccountCredentials.from_json_keyfile_name(cred_path, scopes=scopes)
             with open(pickle_path, "wb") as token:
                 pickle.dump(creds, token)
 
         http_auth = creds.authorize(Http())
-        service = build('sheets', 'v4', http=http_auth)
+        service = build("sheets", "v4", http=http_auth)
         return service
 
     def create_sheet(self, sheet_name):
-        if (self.is_already_sheet(sheet_name)):
+        if self.is_already_sheet(sheet_name):
             return
 
         body = {
-            'requests': [
+            "requests": [
                 {
-                    'addSheet': {
-                        'properties': {
-                            'title': sheet_name,
+                    "addSheet": {
+                        "properties": {
+                            "title": sheet_name,
                         }
                     }
                 }
             ]
         }
-        dst_sheet = self.service.spreadsheets().batchUpdate(spreadsheetId=self.spreadsheet_id, body=body).execute()['replies'][0]['addSheet']
-        template_sheet = [sheet for sheet in self.spreadsheets['sheets'] if 'Template' in sheet['properties']['title']][0]
+        dst_sheet = (
+            self.service.spreadsheets()
+            .batchUpdate(spreadsheetId=self.spreadsheet_id, body=body)
+            .execute()["replies"][0]["addSheet"]
+        )
+        template_sheet = [sheet for sheet in self.spreadsheets["sheets"] if "Template" in sheet["properties"]["title"]][
+            0
+        ]
         self.copy_sheet(template_sheet, dst_sheet)
 
     def copy_sheet(self, src_sheet, dst_sheet):
         body = {
-            'requests': [
+            "requests": [
                 {
-                    'copyPaste': {
-                        'source': {
-                            'sheetId': src_sheet['properties']['sheetId'],
-                            'startRowIndex': 0,
-                            'endRowIndex': src_sheet['properties']['gridProperties']['rowCount'],
-                            'startColumnIndex': 0,
-                            'endColumnIndex': src_sheet['properties']['gridProperties']['columnCount']
+                    "copyPaste": {
+                        "source": {
+                            "sheetId": src_sheet["properties"]["sheetId"],
+                            "startRowIndex": 0,
+                            "endRowIndex": src_sheet["properties"]["gridProperties"]["rowCount"],
+                            "startColumnIndex": 0,
+                            "endColumnIndex": src_sheet["properties"]["gridProperties"]["columnCount"],
                         },
-                        'destination': {
-                            'sheetId': dst_sheet['properties']['sheetId'],
-                            'startRowIndex': 0,
-                            'endRowIndex': dst_sheet['properties']['gridProperties']['rowCount'],
-                            'startColumnIndex': 0,
-                            'endColumnIndex': dst_sheet['properties']['gridProperties']['columnCount']
-                        }
+                        "destination": {
+                            "sheetId": dst_sheet["properties"]["sheetId"],
+                            "startRowIndex": 0,
+                            "endRowIndex": dst_sheet["properties"]["gridProperties"]["rowCount"],
+                            "startColumnIndex": 0,
+                            "endColumnIndex": dst_sheet["properties"]["gridProperties"]["columnCount"],
+                        },
                     }
                 }
             ]
@@ -74,17 +82,16 @@ class GoogleSpreadSheet():
         self.service.spreadsheets().batchUpdate(spreadsheetId=self.spreadsheet_id, body=body).execute()
 
     def is_already_sheet(self, sheet_name):
-        sheet = [sheet for sheet in self.spreadsheets['sheets'] if sheet_name == sheet['properties']['title']]
+        sheet = [sheet for sheet in self.spreadsheets["sheets"] if sheet_name == sheet["properties"]["title"]]
         return len(sheet) != 0
 
     def read_table(self, sheet_name, anchor_col_alpha, anchor_row_num, end_col_alpha, end_row_num):
-        range = f'{sheet_name}!{anchor_col_alpha}{anchor_row_num}:{end_col_alpha}{end_row_num}'
+        range = f"{sheet_name}!{anchor_col_alpha}{anchor_row_num}:{end_col_alpha}{end_row_num}"
         return self.read_values(range)
 
     def read_values(self, range):
-        response = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id,
-                                                            range=range).execute()
-        return response['values']
+        response = self.service.spreadsheets().values().get(spreadsheetId=self.spreadsheet_id, range=range).execute()
+        return response["values"]
 
     def write_table(self, title, df, sheet_name, index_title, anchor_col_alpha, anchor_row_num):
         df_anchor_row_num = str(int(anchor_row_num) + 1)
@@ -97,13 +104,15 @@ class GoogleSpreadSheet():
         self.write_value(title, sheet_name, anchor_col_alpha, anchor_row_num)
         for index, df in enumerate(df_list):
             if index == 0:
-                self.write_dataframe_with_index_columns(df, sheet_name, index_title, anchor_col_alpha, df_index_anchor_row_num)
+                self.write_dataframe_with_index_columns(
+                    df, sheet_name, index_title, anchor_col_alpha, df_index_anchor_row_num
+                )
             else:
                 self.write_dataframe_with_index(df, sheet_name, anchor_col_alpha, df_anchor_row_num)
             df_anchor_row_num = str(int(df_anchor_row_num) + len(df))
 
     def write_value(self, value, sheet_name, col_alpha, row_num):
-        range = f'{sheet_name}!{col_alpha}{row_num}'
+        range = f"{sheet_name}!{col_alpha}{row_num}"
         list = [[value]]
         return self.update_values(range, list)
 
@@ -124,20 +133,19 @@ class GoogleSpreadSheet():
         return self.update_values(range, list)
 
     def update_values(self, range, list):
-        body = {
-            'majorDimension': 'ROWS',
-            'values': list
-        }
-        response = self.service.spreadsheets().values().update(spreadsheetId=self.spreadsheet_id,
-                                                               range=range,
-                                                               valueInputOption='USER_ENTERED',
-                                                               body=body).execute()
-        return response['updatedRange']
+        body = {"majorDimension": "ROWS", "values": list}
+        response = (
+            self.service.spreadsheets()
+            .values()
+            .update(spreadsheetId=self.spreadsheet_id, range=range, valueInputOption="USER_ENTERED", body=body)
+            .execute()
+        )
+        return response["updatedRange"]
 
     def alpha2num(self, alpha):
         num = 0
         for index, item in enumerate(list(alpha)):
-            num += pow(26, len(alpha) - index - 1) * (ord(item) - ord('A') + 1)
+            num += pow(26, len(alpha) - index - 1) * (ord(item) - ord("A") + 1)
         return num
 
     def num2alpha(self, num):
@@ -153,5 +161,5 @@ class GoogleSpreadSheet():
         len_row = len(list)
         len_col = [len(oneline) for oneline in list][0]
         end = self.num2alpha(anchor_col_num_int + len_col) + str(int(anchor_row_num) + len_row)
-        range = f'{sheet_name}!{anchor_col_alpha}{anchor_row_num}:{end}'
+        range = f"{sheet_name}!{anchor_col_alpha}{anchor_row_num}:{end}"
         return range
